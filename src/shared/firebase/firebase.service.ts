f0e661213be22ff8bfe52e6f9280675cf2e2c952
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2024-10-13 18:19:53 +0300

    change firebase settings

--- src/shared/firebase/firebase.service.ts
+++ src/shared/firebase/firebase.service.ts
@@ -1,16 +1,15 @@
 import * as admin from 'firebase-admin';
 import { Injectable } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
-import serviceAccount from './linkedin-285d2-firebase-adminsdk-4k8jd-c8a94afca0.json';
-import { ServiceAccount } from 'firebase-admin';
-const serviceAccountKey = serviceAccount as ServiceAccount;
+
+const firebaseConfig = JSON.parse(process.env.FIREBASE_ADMIN_SDK);
 
 @Injectable()
 export class FirebaseService {
   constructor() {
     if (!admin.apps.length) {
       admin.initializeApp({
-        credential: admin.credential.cert(serviceAccountKey),
+        credential: admin.credential.cert(firebaseConfig),
         storageBucket: 'gs://linkedin-285d2.appspot.com',
       });
     }
