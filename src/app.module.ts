b5887fd1532916f96017f2aa19e06c5b0ef3c18b
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2024-10-17 22:59:56 +0300

    change url

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,11 +25,7 @@ import { ActiveConversationEntity } from './chat/entities/active-conversation';
     PostsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.POSTGRES_DB,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: false,
       ssl: {
